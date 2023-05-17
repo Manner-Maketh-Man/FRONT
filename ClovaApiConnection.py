9cb54693ca4de6211eac9140929e98256d9f7f67
JWseop <tjqdnwj@gmail.com>
2023-05-17 15:52:28 +0900

    feat[wooseop]: serverRequest 함수 구현 완료

--- ClovaApiConnection.py
+++ ClovaApiConnection.py
@@ -4,33 +4,32 @@ import time
 import base64
 import json
 
-api_url = 'https://6vscc0s85m.apigw.ntruss.com/custom/v1/22615/bfb5ec99eba9f8ad32b3178b52c96769efcf7ebb0e172be48d160e085a55c1db/general'
-secret_key = 'YmN5dlRVRHBseFFmQVN3bWRnR1NYSHpjZ2plV2RQU3o='
-# image_url = ''
+def send_OCR_request(image_file):
+    api_url = 'https://6vscc0s85m.apigw.ntruss.com/custom/v1/22615/bfb5ec99eba9f8ad32b3178b52c96769efcf7ebb0e172be48d160e085a55c1db/general'
+    secret_key = 'YmN5dlRVRHBseFFmQVN3bWRnR1NYSHpjZ2plV2RQU3o='
 
-image_file = 'ScreenShot_img/IMGforOCR.jpg'
-with open(image_file,'rb') as f:
-    file_data = f.read()
+    with open(image_file,'rb') as f:
+        file_data = f.read()
 
-request_json = {
-    'images': [
-        {
-            'format': 'jpg',
-            'name': 'demo',
-            'data': base64.b64encode(file_data).decode(),
-            # 'url': image_url
-        }
-    ],
-    'requestId': str(uuid.uuid4()),
-    'version': 'V2',
-    'timestamp': int(round(time.time() * 1000))
-}
-payload = json.dumps(request_json).encode('UTF-8')
-headers = {
-  'X-OCR-SECRET': secret_key,
-  'Content-Type': 'application/json'
-}
+    request_json = {
+        'images': [
+            {
+                'format': 'jpg',
+                'name': 'demo',
+                'data': base64.b64encode(file_data).decode(),
+            }
+        ],
+        'requestId': str(uuid.uuid4()),
+        'version': 'V2',
+        'timestamp': int(round(time.time() * 1000))
+    }
 
-response = requests.request("POST", api_url, headers=headers, data = payload)
+    payload = json.dumps(request_json).encode('UTF-8')
+    headers = {
+      'X-OCR-SECRET': secret_key,
+      'Content-Type': 'application/json'
+    }
 
-print(response.text)
+    response = requests.request("POST", api_url, headers=headers, data = payload)
+    
+    return response
--- serverRequest.py
+++ serverRequest.py
@@ -1,24 +1,33 @@
 import requests
 import json
+import ClovaApiConnection
 
-# 서버 URL
-url = 'https://c774-211-198-109-254.ngrok-free.app/apis/process_file/'
+def send_request():
+    # 서버 URL
+    url = 'https://c774-211-198-109-254.ngrok-free.app/apis/process_file/'
 
-# 보낼 데이터
-data = {}
+    # 함수 호출
+    image_file = 'ScreenShot_img/IMGforOCR.jpg'
+    OCR_response = ClovaApiConnection.send_OCR_request(image_file)
+    
+    # 보낼 데이터
+    # data = {"version":"V2","requestId":"9425f4c3-9fc2-40cf-882c-2fcdeea2eecf","timestamp":168428881140,}
 
-# 데이터를 JSON 형식으로 변환
-json_data = json.dumps(data)
+    # # # 데이터를 필요 시 JSON 형식으로 변환
+    json_data = OCR_response.json()
 
-# 요청 헤더 정의 (JSON 형식의 데이터를 보내기 때문에 'Content-Type'을 'application/json'으로 설정)
-headers = {'Content-Type': 'application/json'}
+    # 요청 헤더 정의 (JSON 형식의 데이터를 보내기 때문에 'Content-Type'을 'application/json'으로 설정)
+    headers = {'Content-Type': 'application/json'}
 
-# POST 요청
-response = requests.post(url, data=json_data, headers=headers)
+    # POST 요청
+    response = requests.post(url, data=json.dumps(json_data), headers=headers)
 
-# 응답 출력 (JSON 형식일 경우)
-if response.status_code == 200:
-    response_data = response.json()
-    print(response_data)
-else:
-    print("Error:", response.status_code)
+    # 응답 출력 (JSON 형식일 경우)
+    if response.status_code == 200:
+        response_data = response.json()
+        print(response_data)
+    else:
+        print("Error:", response.status_code)
+
+# 함수 호출
+send_request()
